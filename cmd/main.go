e9d55f01fb358765b5d3990fcd77b3371a636a4d
oloomoses <olooine@gmail.com>
2025-12-14 11:34:55 +0300

    Configure and instantiate db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,55 @@
-package cmd
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/oloomoses/opinions-hub/internal/config"
+	"github.com/oloomoses/opinions-hub/internal/database"
+)
+
+func main() {
+
+	if err := config.Load(); err != nil {
+		log.Fatal("Failed to load config", err)
+	}
+
+	db, err := database.Connect()
+
+	if err != nil {
+		log.Fatal("db error: ", err)
+	}
+
+	defer db.Close()
+
+	srv := &http.Server{
+		Addr: ":8080",
+	}
+
+	go func() {
+		log.Println("server started")
+
+		if err := srv.ListenAndServe(); err != nil {
+			log.Print(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	log.Println("shutting down......")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	srv.Shutdown(ctx)
+}
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,7 +1,45 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+var DB *sql.DB
 
 func Connect() (*sql.DB, error) {
 
+	dns := fmt.Sprintf(
+		"postgres://%s:%s@localhost:5432/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	fmt.Println(dns)
+
+	db, err := sql.Open("pgx", dns)
+
+	if err != nil {
+		return nil, err
+	}
+
+	maxConns := 20
+	maxIdle := 10
+
+	db.SetMaxOpenConns(maxConns)
+	db.SetMaxIdleConns(maxIdle)
+	db.SetConnMaxLifetime(30 * time.Minute)
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	DB = db
+
+	return db, nil
 }
